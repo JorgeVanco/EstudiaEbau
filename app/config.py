d545e6dffe2f1deb7f2bf37b3e350576c68d0d77
JorgeVanco <jorge.vanco.sampedro@gmail.com>
2024-10-22 13:21:56 +0200

    Made it work on azure

--- app/config.py
+++ app/config.py
@@ -1,19 +1,20 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-  admin_email: str
-  secret_key: str
-  algorithm: str
-  access_token_expire_minutes: int
-  my_password: str
-  database_hostname: str
-  database_port: str
-  database_password: str
-  database_name: str
-  database_username: str
+    admin_email: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+    my_password: str
+    database_hostname: str
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
 
-  class Config:
-    env_file = ".env"
+    class Config:
+        env_file = ".env"
+
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 
-from .oauth2 import  get_current_user
-from .data_functions import add_tema_to_file, get_data_from_file, add_question_to_file, add_subject_to_file, add_image_to_file
+from .oauth2 import get_current_user
+from .data_functions import (
+    add_tema_to_file,
+    get_data_from_file,
+    add_question_to_file,
+    add_subject_to_file,
+    add_image_to_file,
+)
 from .config import settings
 from . import schemas
 from . import auth, users
@@ -14,11 +20,10 @@ app = FastAPI()
 origins = [
     "http://localhost",
     "http://localhost:3000",
-
-    "http://localhost:8000", 
+    "http://localhost:8000",
     "http://127.0.0.1:8000",
     "https://school-study-a01d2.web.app",
-    "https://estudia-ebau.web.app"
+    "https://estudia-ebau.web.app",
 ]
 
 app.add_middleware(
@@ -32,46 +37,64 @@ app.add_middleware(
 app.include_router(auth.router)
 app.include_router(users.router)
 
+
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+
 @app.get("/data")
 async def get_data():
-  data = get_data_from_file() 
-  return data
+    data = get_data_from_file()
+    return data
+
 
 @app.post("/add-question")
-async def add_question(question_request: schemas.Question_request, current_user: dict = Depends(get_current_user)):
-  if current_user["user_email"] != settings.admin_email: return {"Error": "Not authorized"}
-  if len(question_request.question.answers) != 3: return {"Error": "Not valid length"}
-  print(question_request.dict())
-  response = add_question_to_file(question_request.dict())
-  return response
+async def add_question(
+    question_request: schemas.Question_request,
+    current_user: dict = Depends(get_current_user),
+):
+    if current_user["user_email"] != settings.admin_email:
+        return {"Error": "Not authorized"}
+    if len(question_request.question.answers) != 3:
+        return {"Error": "Not valid length"}
+    print(question_request.dict())
+    response = add_question_to_file(question_request.dict())
+    return response
 
 
 @app.post("/add-tema")
-def add_new_tema(tema_request: schemas.Tema_request, current_user: dict = Depends(get_current_user)):
-  if current_user["user_email"] != settings.admin_email: return {"Error": "Not authorized"}
-  response = add_tema_to_file(tema_request.dict())
-  return response
+def add_new_tema(
+    tema_request: schemas.Tema_request, current_user: dict = Depends(get_current_user)
+):
+    if current_user["user_email"] != settings.admin_email:
+        return {"Error": "Not authorized"}
+    response = add_tema_to_file(tema_request.dict())
+    return response
+
 
 @app.post("/add-subject")
-def add_new_subject(subject_request: schemas.Subject_request, current_user: dict = Depends(get_current_user)):
-  if current_user["user_email"] != settings.admin_email: return {"Error": "Not authorized"}
-  print("Trying to add new subject")
-  response = add_subject_to_file(subject_request.dict())
-  return response
+def add_new_subject(
+    subject_request: schemas.Subject_request,
+    current_user: dict = Depends(get_current_user),
+):
+    if current_user["user_email"] != settings.admin_email:
+        return {"Error": "Not authorized"}
+    print("Trying to add new subject")
+    response = add_subject_to_file(subject_request.dict())
+    return response
+
 
 @app.post("/email")
 def get_email(email_request: schemas.Email):
-  return {"isAdmin": email_request.email == settings.admin_email}
+    return {"isAdmin": email_request.email == settings.admin_email}
+
 
 @app.post("/image")
 def add_image(img: schemas.Image, current_user: dict = Depends(get_current_user)):
-  if current_user["user_email"] != settings.admin_email: return {"Error": "Not authorized"}
-  return add_image_to_file(img)
-
+    if current_user["user_email"] != settings.admin_email:
+        return {"Error": "Not authorized"}
+    return add_image_to_file(img)
 
 
 # @app.post("/login", response_model=schemas.Token)
@@ -80,7 +103,7 @@ def add_image(img: schemas.Image, current_user: dict = Depends(get_current_user)
 #   #user = db.query(Users).filter(Users.email == user_credentials.username).first()
 
 #   if user_credentials.username == settings.admin_email and user_credentials.password == settings.my_password:
-#     access_token = create_access_token(data= {"user_email": user_credentials.username}) 
+#     access_token = create_access_token(data= {"user_email": user_credentials.username})
 #     return {"access_token": access_token, "token_type": "bearer"}
 #   else:
 #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid Credentials")
